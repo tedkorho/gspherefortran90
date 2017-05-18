75fba01d58c011545304a26d4b171472f86e1c4e
tedkorho <teo.i.korhonen@helsinki.fi>
2017-05-18 10:29:45 +0300

    Added test file

---
+++ test.py
@@ -0,0 +1,6 @@
+from subprocess import call
+
+call(['valgrind','./gsphere','input/test1.in'])
+call(['valgrind','./gsphere','input/test2.in'])
+call(['valgrind','./gsphere','input/test3.in'])
+call(['valgrind','./gsphere','input/test4.in'])
